38b8fa54d7a7403bf659bc5b847ed0df4082ced6
odair2001 <odairfelix2001@gmail.com>
2026-02-22 14:08:05 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,16 +1,6 @@
-const supabaseUrl = "https://vvdhaxtqelnjpwrgtlod.supabase.co";
-const supabaseKey = "sb_publishable_UTbX94YYQlpbrYsep9gi2Q_L36njeA-";
-
-const supabaseClient = supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-
-);const supabaseUrl = "https://vvdhaxtqelnjpwrgtlod.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2ZGhheHRxZWxuanB3cmd0bG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2OTkyNTQsImV4cCI6MjA4NzI3NTI1NH0.fJPNdd8VxXNpTEumR4OUv0mwGCZCvhUF3iBhvMA3Vn8"; // use a anon public, não publishable
+// supabase.js
+window.supabaseUrl = "https://vvdhaxtqelnjpwrgtlod.supabase.co";
+window.supabaseKey = "sb_publishable_UTbX94YYQlpbrYsep9gi2Q_L36njeA-";
 
 const { createClient } = window.supabase;
-
-const supabaseClient = createClient(
-  supabaseUrl,
-  supabaseKey
-);
+window.supabaseClient = createClient(window.supabaseUrl, window.supabaseKey);
